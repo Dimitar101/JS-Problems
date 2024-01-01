c7b3c048aabf2b9c643256afc4810d224a19667f
Dimitar K Dimitrov <150704248+Dimitar101@users.noreply.github.com>
2024-01-01 16:48:32 +0200

    Add files via upload

---
+++ Folder/1. Array Rotation.js
@@ -0,0 +1,12 @@
+function solve(array, rotations_n) {
+    const arrLen = array.length;
+    for (let i = 0; i < rotations_n % arrLen; i++) {
+        array.push(array.shift());
+    }
+    console.log(array.join(' '));
+}
+
+
+solve([51, 47, 32, 61, 21], 2)
+solve([32, 21, 61, 1], 4)
+solve([2, 4, 15, 31], 5)
